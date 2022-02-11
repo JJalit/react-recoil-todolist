2e8918e7008b7d7d071ab22e297a868dddea2827
jaeha <jaeha23@naver.com>
2022-02-11 14:24:22 +0900

    create TodoItemCreator

---
+++ src/TodoList/TodoItemCreator/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+
+import { getId } from '../utils';
+import { todoListState } from '../state';
+
+function TodoItemCreator() {
+  const [inputValue, setInputValue] = useState<string>('');
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const addItem = () => {
+    setTodoList(oldTodoList => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue('');
+  };
+
+  const onChange = ({ target: { value } }) => {
+    setInputValue(value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+}
+
+export default TodoItemCreator;
